eb58e286ea20b771378e12b766031f73a21e8bbd
Denisse Valle <76190107+Denisse-Valle@users.noreply.github.com>
2023-12-03 22:19:55 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,240 +1,254 @@
-#include<iostream>
-#include<cstdlib>
-#include<string>
-#include<cstdio>
-#include<vector>
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <algorithm> 
 using namespace std;
+
 const int TABLE_SIZE = 10;
-class HashNode{
-  public:
-      int key;
-      string value;
-      HashNode* next;
-      HashNode(int key, string value){
-        this->key = key;
-        this->value = value;
-        this->next = NULL;
-      }
+
+class HashNode {
+public:
+    int key;
+    string value;
+    HashNode* next;
+
+    HashNode(int key, string value) : key(key), value(value), next(nullptr) {}
 };
 
-int Part(vector<string> &arr, int start, int end) {
+int Part(vector<string>& arr, int start, int end) {
+    string pivot = arr[end];
+    int pIndex = start;
 
-  string pivot = arr[end];
-  int pIndex = start;
-  for (int i = start; i < end; i++) {
-    if (arr[i] <= pivot) {
-      swap(arr[i], arr[pIndex]);
-      pIndex++;
+    for (int i = start; i < end; i++) {
+        if (arr[i] <= pivot) {
+            swap(arr[i], arr[pIndex]);
+            pIndex++;
+        }
     }
-  }
-  swap(arr[pIndex], arr[end]);
-  return pIndex;
-}
-void QS(vector<string> &arr, int start, int end) {
 
-  if (start < end) {
-    int pivot = Part(arr, start, end);
-    QS(arr, start, pivot - 1);
-    QS(arr, pivot + 1, end);
-  }
+    swap(arr[pIndex], arr[end]);
+    return pIndex;
 }
 
-class HashMap{
-  private:
-  HashNode** htable;
-  public:
-  void DisplayHash(){
-    for (int i=0; i < TABLE_SIZE; i++){
-      if (htable[i] != NULL){
-        cout << "Index " << i << ": ";
-      }
-        HashNode* ptr = htable[i];
-        while (ptr != NULL){
-        cout << "(" << ptr->key << ", " << ptr->value << ")";
-        ptr = ptr->next;
-        if (ptr != NULL){ 
-        cout << "->";}  
-        else{
-        cout << endl;}
+void QS(vector<string>& arr, int start, int end) {
+    if (start < end) {
+        int pivot = Part(arr, start, end);
+        QS(arr, start, pivot - 1);
+        QS(arr, pivot + 1, end);
+    }
+}
+
+class HashMap {
+private:
+    HashNode** htable;
+
+public:
+    HashMap() {
+        htable = new HashNode*[TABLE_SIZE]();
+    }
+
+    ~HashMap() {
+        for (int i = 0; i < TABLE_SIZE; ++i) {
+            HashNode* entry = htable[i];
+            while (entry != nullptr) {
+                HashNode* prev = entry;
+                entry = entry->next;
+                delete prev;
+            }
+        }
+        delete[] htable;
+    }
+
+    int HashFunc(int key) {
+        return key % TABLE_SIZE;
+    }
+
+    void Insert(int key, string value) {
+        int hash_val = HashFunc(key);
+        HashNode* prev = nullptr;
+        HashNode* entry = htable[hash_val];
+
+        while (entry != nullptr) {
+            prev = entry;
+            entry = entry->next;
         }
 
+        if (entry == nullptr) {
+            entry = new HashNode(key, value);
+            if (prev == nullptr) {
+                htable[hash_val] = entry;
+            }
+            else {
+                prev->next = entry;
+            }
+        }
+        else {
+            entry->value = value;
+        }
     }
 
+    void Remove(int key) {
+        int hash_val = HashFunc(key);
+        HashNode* entry = htable[hash_val];
+        HashNode* prev = nullptr;
 
+        while (entry != nullptr && entry->key != key) {
+            prev = entry;
+            entry = entry->next;
+        }
 
-  }
-  HashMap(){
-      htable = new HashNode*[TABLE_SIZE];
-      for (int i = 0; i < TABLE_SIZE; i++){
-          htable[i] = NULL;
-  }
-  }
-  ~HashMap(){
-      for (int i = 0; i < TABLE_SIZE; ++i){
-          HashNode* entry = htable[i];
-          while (entry != NULL){
-              HashNode* prev = entry;
-              entry = entry->next;
-              delete prev;
-              }
-      }
-      delete[] htable;
-  }
+        if (entry == nullptr) {
+            cout << "No hay nada en la posición " << key << endl;
+            return;
+        }
 
-  int HashFunc(int key){
-      return key % TABLE_SIZE;
-  }
+        if (prev != nullptr) {
+            prev->next = entry->next;
+        }
+        else {
+            htable[hash_val] = entry->next;
+        }
 
-
-  void Insert(int key, string value){
-      int hash_val = HashFunc(key);
-      HashNode* prev = NULL;
-      HashNode* entry = htable[hash_val];
-      while (entry != NULL){
-          prev = entry;
-          entry = entry->next;
-      }
-      if (entry == NULL){
-          entry = new HashNode(key, value);
-          if (prev == NULL){
-              htable[hash_val] = entry;
-          }
-    else{
-              prev->next = entry;
-          }
-      }
-      else{
-          entry->value = value;
-      }
-  }
-
-  void Remove(int key){
-      int hash_val = HashFunc(key);
-      HashNode* entry = htable[hash_val];
-      HashNode* prev = NULL;
-      if (entry == NULL || entry->key != key){
-        cout<<"No hay nada en  "<<key<<endl;
-          return;
-      }
-      while (entry->next != NULL){
-          prev = entry;
-          entry = entry->next;
-      }
-      if (prev != NULL){
-          prev->next = entry->next;
-      }
-      delete entry;
-      cout<<"Elemento borrado"<<endl;
-  }
-
-  int Search(int key){
-      bool flag = false;
-      int hash_val = HashFunc(key);
-      HashNode* entry = htable[hash_val];
-      while (entry != NULL){
-          if (entry->key == key){
-              cout<<entry->value<<" ";
-              flag = true;
-          }
-          entry = entry->next;
-      }
-      if (!flag){
-          return -1;
-      }
+        delete entry;
+        cout << "Elemento borrado exitosamente" << endl;
     }
 
+    void DisplayHash() {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        HashNode* ptr = htable[i];
+        if (ptr != nullptr) {
+            cout << "Index " << i << ": ";
 
-  int QuickSort(int key){
-      vector<string> v;
-      bool flag = false;
-      int hash_val = HashFunc(key);
-      HashNode* entry = htable[hash_val];
-      cout<<"Lista original\n";
-      while (entry != NULL){
-          if (entry->key == key){
-              cout<< entry->value<<" ";
-              v.push_back(entry->value);
-              flag = true;
-          }
-          entry = entry->next;
-      }
-        cout << endl;
-        QS(v, 0, v.size()-1);
-        cout << endl;
-        cout<<"Vector ordenado\n";
-        for(string element:v)
-        cout << element << " ";
+            while (ptr != nullptr) {
+                cout << "(" << ptr->key << ", " << ptr->value << ")";
+                ptr = ptr->next;
 
-      if (!flag){
-          return -1;
-      }
-  }
+                if (ptr != nullptr) {
+                    cout << " -> ";
+                }
+            }
+            cout << endl;
+        }
+    }
+}
+
+    int Search(int key) {
+        bool flag = false;
+        int hash_val = HashFunc(key);
+        HashNode* entry = htable[hash_val];
+
+        while (entry != nullptr) {
+            if (entry->key == key) {
+                cout << entry->value << " ";
+                flag = true;
+            }
+            entry = entry->next;
+        }
+
+        if (!flag) {
+            return -1;
+        }
+
+        return 0;
+    }
+
+    void QuickSort(int key) {
+        vector<string> v;
+        bool flag = false;
+        int hash_val = HashFunc(key);
+        HashNode* entry = htable[hash_val];
+
+        cout << "Lista original\n";
+        while (entry != nullptr) {
+            if (entry->key == key) {
+                cout << entry->value << " ";
+                v.push_back(entry->value);
+                flag = true;
+            }
+            entry = entry->next;
+        }
+
+        cout << endl;
+        QS(v, 0, v.size() - 1);
+
+        cout << "Lista ordenada\n";
+        for (string element : v)
+            cout << element << " ";
+
+        cout << endl;
+
+        if (!flag) {
+            cout << "No hay nada en la posicion " << key << endl;
+        }
+    }
 };
 
-
-
-
-int main(){
+int main() {
     HashMap hash;
     int key;
     string value;
     int choice;
-    while(1){
-        cout<<"\n----------------------"<<endl;
-        cout<<"Aplicacion de contactos"<<endl;
-        cout<<"\n----------------------"<<endl;
-        cout<<"1.Insertar elemento a la tabla"<<endl;
-        cout<<"2.Buscar elemento por llave"<<endl;
-        cout<<"3.Borrar elemento por llave"<<endl;
-        cout<<"4. Mostra tabla"<<endl;
-        cout <<"5. Ordenar listas"<< endl;
-        cout<<"6.Salir"<<endl;
-        cout<<"Tu opcion: ";
-        cin>>choice;
-        switch(choice){
+    int bandera = 1;
+
+    do {
+        cout << "\n----------------------" << endl;
+        cout << "Aplicacion de contactos" << endl;
+        cout << "----------------------" << endl;
+        cout << "1. Insertar elemento a la tabla" << endl;
+        cout << "2. Buscar elemento por llave" << endl;
+        cout << "3. Borrar elemento por llave" << endl;
+        cout << "4. Mostrar tabla" << endl;
+        cout << "5. Ordenar listas" << endl;
+        cout << "6. Salir" << endl;
+        cout << "Tu opcion: ";
+        cin >> choice;
+
+        switch (choice) {
         case 1:
-            cout<<"Inserta un elemento de la tabla: ";
-            cin>>value;
-            cout<<"Inserta la llave para el elemento: ";
-            cin>>key;
+            cout << "Inserta un elemento de la tabla: ";
+            cin >> value;
+            cout << "Inserta la llave para el elemento: ";
+            cin >> key;
             hash.Insert(key, value);
             break;
+
         case 2:
-            cout<<"Inserta la llave del elemento buscado ";
-            cin>>key;
-            cout<<"Elemento en  "<<key<<" : ";
-            if (hash.Search(key) == -1){
-            cout<<"No hay nada en la posicion "<<key<<endl;
-            }continue;
+            cout << "Inserta la llave del elemento buscado: ";
+            cin >> key;
+            cout << "Elemento en " << key << ": ";
+            if (hash.Search(key) == -1) {
+                cout << "No hay nada en la posición " << key << endl;
+            }
             break;
+
         case 3:
-            cout<<"Ingresa la llave que deseas remover: ";
-            cin>>key;
+            cout << "Ingresa la llave que deseas remover: ";
+            cin >> key;
             hash.Remove(key);
             break;
-        case 4:
 
-            cout<<"La tabla es: "<<endl;
+        case 4:
+            cout << "La tabla es: " << endl;
             hash.DisplayHash();
             break;
 
         case 5:
-            cout << "Dame el indice para ordenar: " << endl;
+            cout << "Dame el índice para ordenar: ";
             cin >> key;
             hash.QuickSort(key);
-            main();
-
-
+            break;
 
         case 6:
-            exit(1);
+            bandera = 0;
+            break;
 
         default:
-           cout<<"\nSelecciona una opcion valida\n";
+            cout << "\nSelecciona una opción válida\n";
         }
 
-    }   
-        return 0;
+    } while (bandera);
 
-
+    return 0;
 }
+
